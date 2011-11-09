0f4c570d417632592c6831b0d0c9204652b8938e
Joony <jonathan.mcallister@gmail.com>
2011-11-09 10:42:45 +0100

    Initial Commit

---
+++ Basic Template/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  Basic Template
+//
+//  Created by Jonathan McAllister on 09/11/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Basic Template/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Basic Template
+//
+//  Created by Jonathan McAllister on 09/11/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
