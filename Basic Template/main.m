4bd8e488add9996b17d917b6499805d7225ee93b
Joony <jonathan.mcallister@gmail.com>
2011-11-09 13:37:03 +0100

    Initial Import

--- Basic Template/main.m
+++ Basic Template/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  Basic Template
 //
-//  Created by Jonathan McAllister on 09/11/2011.
+//  Created by Joony on 09/11/2011.
 //  Copyright __MyCompanyName__ 2011. All rights reserved.
 //
 
